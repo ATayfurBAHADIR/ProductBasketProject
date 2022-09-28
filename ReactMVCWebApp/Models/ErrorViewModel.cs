c50ee11d4a558d976a40856abda6dd0099a08cba
ATayfurBAHADIR <a.tayfur.bahadir@outlook.com>
2022-09-28 20:15:37 +0300

    React Web Uygulaması eklendi.

---
+++ ReactMVCWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReactMVCWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
